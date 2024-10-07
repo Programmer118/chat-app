77231158d03b757c5f2365a31b78a0c4c6f99a2d
Shivam singh <ss8700977@gmail.com>
2024-10-07 15:22:18 +0530

    socket url update

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -17,7 +17,7 @@ export const SocketContextProvider = ({children})=>{
 
     useEffect(() => {
         if(authUser){
-            const socket = io("http://localhost:5050",{
+            const socket = io("https://chat-app-szsw.onrender.com",{
                 query:{
                     userId:authUser.user.id,
                 }
